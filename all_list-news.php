e9ac70e1bd5c748a8464116ebb2614423283fce2
Mogrima <rebel.rs@yandex.ru>
2018-12-17 21:57:58 +0500

    Выводит все новости списком из заголовков и изображений, он же посылает данные другому сценарию, который открывает новость целиком для просмотра и возможного последующего удаления

---
+++ all_list-news.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Обзор новостей</title>
+</head>
+<body>
+	<?php
+	require_once('appvars.php');
+    require_once('connectvars.php');
+
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    $query = "SELECT * FROM news ORDER BY date ASC";
+    $data = mysqli_query($dbc, $query);
+
+    while($row = mysqli_fetch_array($data)) {
+    	echo '<div>';
+    	echo '<h3>' . $row['title'] . '</h3>';
+    	echo '<time>' . $row['date'] . '</time>';
+    	echo '<img src="' . G_UPLOADPATH . $row['screenshot'] . '">';
+    	echo '<a href="prewievs_news.php?id=' . $row['id'] . '">Просмотреть</a>';
+    	echo '</div>';
+    }
+  ?>
+</body>
+</html>
